ca83b56ac217b298c2c752b03d273528f8878fe0
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-12-12 17:48:57 -0500

    added information to the models seedDB

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,16 +3,19 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
-		description: "This is a school in NYC."
+		description: "This is a school in NYC.",
+		address: "695 Park Ave, New York, NY 10065"
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
-		description: "This is a school in MA."
+		description: "This is a school in MA.",
+		address: "86 Brattle Street Cambridge, MA 02138"
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
       lastname: "Shmo",
+	  email: "joeshmo@abc.com"
 		});
 
 	await dummy_student.setCampus(dummy_campus);
