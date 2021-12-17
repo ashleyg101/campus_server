069a30b0c13e0deff89384093f83744301cddf82
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-12-16 19:22:37 -0500

    campus images

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,11 +3,13 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
+		imageURL: "https://nypost.com/wp-content/uploads/sites/2/2020/10/012420Starbucks7HS.jpg?quality=90&strip=all&w=1488",
 		description: "This is a school in NYC.",
 		address: "695 Park Ave, New York, NY 10065"
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		imageURL: "https://image.cnbcfm.com/api/v1/image/106604493-1594057373282harvarduniversitysdunsterhouseincambridgema.jpg?v=1594057738&w=630&h=354",
 		description: "This is a school in MA.",
 		address: "86 Brattle Street Cambridge, MA 02138"
 	});
